dfa3bd0032b0881577fd8e540c52fa634e1ed33d
aabhinavvvvvvv <abhinavguptaxia9213@gmail.com>
2025-06-09 06:19:31 +0530

    done

--- C_Cool_Partition.cpp
+++ C_Cool_Partition.cpp
@@ -6,14 +6,20 @@ int main()
     while(t--){
         int n;cin>>n;
         vector<int> v(n);
-        unordered_map<int,int> mp;
         for(auto&x :v){
             cin>>x;
-            mp[x]++;
         }
+        set<int> prev,total;
         int ans=0;
-        for(auto x:mp){
-            ans=max(ans,x.second);
+        for(auto x:v){
+            total.insert(x);
+            if(prev.count(x)){
+                prev.erase(x);
+            }
+            if(prev.empty()){
+                ans++;
+                prev=total;
+            }
         }
         cout<<ans<<endl;
     }
