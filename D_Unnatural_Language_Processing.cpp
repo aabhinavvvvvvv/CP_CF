c716572cd26adf49a2e8af04af3a78170afe8867
aabhinavvvvvvv <abhinavguptaxia9213@gmail.com>
2025-06-23 16:28:32 +0530

    cf 918 div4

---
+++ D_Unnatural_Language_Processing.cpp
@@ -0,0 +1,101 @@
+/*
+ * Competitive Programming Template
+ * Author: Abhinav Gupta
+ * GitHub: @aabhinavvvvvvv
+ * MAHAKAL KI JAI
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 5;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define F first
+#define S second
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+#define per(i,a,b) for(int i=a; i>b; --i)
+#define each(x,a) for(auto &x : a)
+#define sz(x) (int)(x).size()
+
+void fastIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = "; _print(x); cerr << '\n';
+#else
+#define debug(x)
+#endif
+
+template<typename T> void _print(T x) { cerr << x; }
+template<typename T, typename V> void _print(pair<T, V> p) {
+    cerr << '{'; _print(p.first); cerr << ", "; _print(p.second); cerr << '}';
+}
+template<typename T> void _print(vector<T> v) {
+    cerr << '[';
+    for (auto &i : v) _print(i), cerr << ' ';
+    cerr << ']';
+}
+template<typename T> void _print(set<T> s) {
+    cerr << '{';
+    for (auto &i : s) _print(i), cerr << ' ';
+    cerr << '}';
+}
+template<typename K, typename V> void _print(map<K, V> m) {
+    cerr << '{';
+    for (auto &p : m) _print(p), cerr << ' ';
+    cerr << '}';
+}
+
+/*
+ * Bakchodi Mat Kar Laude
+ * Chup Chap code kar
+ * I will not be responsible for any damage caused by this code
+ */
+void solve() {
+    int n; cin>>n;
+    string s; cin>>s;
+    int i;
+    for(i=0;i<n && i+3<n;){
+        if(s[i+3] == 'a' || s[i+3] == 'e'){
+            cout<<s.substr(i,2);
+            i+=2;
+        }
+        else{
+            cout<<s.substr(i,3);
+            i+=3;
+        }
+        cout<<".";
+    }
+    if(i+3<n){
+        cout<<s.substr(i,2);
+    }
+    else{
+        cout<<s.substr(i,3);
+    }
+    cout<<endl;
+
+}
+
+int main() {
+    fastIO();
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
---
+++ E_Romantic_Glasses.cpp
@@ -0,0 +1,102 @@
+/*
+ * Competitive Programming Template
+ * Author: Abhinav Gupta
+ * GitHub: @aabhinavvvvvvv
+ * MAHAKAL KI JAI
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 5;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define F first
+#define S second
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+#define per(i,a,b) for(int i=a; i>b; --i)
+#define each(x,a) for(auto &x : a)
+#define sz(x) (int)(x).size()
+
+void fastIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = "; _print(x); cerr << '\n';
+#else
+#define debug(x)
+#endif
+
+template<typename T> void _print(T x) { cerr << x; }
+template<typename T, typename V> void _print(pair<T, V> p) {
+    cerr << '{'; _print(p.first); cerr << ", "; _print(p.second); cerr << '}';
+}
+template<typename T> void _print(vector<T> v) {
+    cerr << '[';
+    for (auto &i : v) _print(i), cerr << ' ';
+    cerr << ']';
+}
+template<typename T> void _print(set<T> s) {
+    cerr << '{';
+    for (auto &i : s) _print(i), cerr << ' ';
+    cerr << '}';
+}
+template<typename K, typename V> void _print(map<K, V> m) {
+    cerr << '{';
+    for (auto &p : m) _print(p), cerr << ' ';
+    cerr << '}';
+}
+
+/*
+ * Bakchodi Mat Kar Laude
+ * Chup Chap code kar
+ * I will not be responsible for any damage caused by this code
+ */
+void solve() {
+    int n;
+        cin >> n;
+        vector<int> v(n);
+        rep(i,0,n){
+            cin>>v[i];
+            if(i&1){
+                v[i]*=-1;
+            }
+        }
+        set<ll> st;
+        ll sum =0;
+        st.insert(0);
+        rep(i,0,n){
+            sum+=v[i];
+            if(st.count(sum)){
+                cout<<"YES"<<endl;
+                return;
+            }
+            st.insert(sum);
+        }
+        cout<<"NO"<<endl;
+        // cout << (ok ? "YES" : "NO") << '\n';
+    
+}
+
+int main() {
+    fastIO();
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
