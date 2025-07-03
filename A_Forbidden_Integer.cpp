6cabb5f56d1748b3c1b4a9ddc08e31e1bb63ad7f
aabhinavvvvvvv <abhinavguptaxia9213@gmail.com>
2025-07-04 03:30:43 +0530

    some shit

---
+++ A_Forbidden_Integer.cpp
@@ -0,0 +1,113 @@
+/*
+ * Competitive Programming Template
+ * Author: Abhinav Gupta
+ * GitHub: @aabhinavvvvvvv
+ * MAHAKAL KI JAI
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 5;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define F first
+#define S second
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+#define per(i,a,b) for(int i=a; i>b; --i)
+#define each(x,a) for(auto &x : a)
+#define sz(x) (int)(x).size()
+#define fastIO() ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+
+void _print(int t) { cerr << t; }
+void _print(long long t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
+void _print(string t) { cerr << '"' << t << '"'; }
+void _print(char t) { cerr << '\'' << t << '\''; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+
+template <typename T, typename V> void _print(pair<T, V> p);
+template <typename T> void _print(vector<T> v);
+template <typename T> void _print(set<T> v);
+template <typename T> void _print(multiset<T> v);
+template <typename T, typename V> void _print(map<T, V> v);
+
+template <typename T, typename V>
+void _print(pair<T, V> p) { cerr << '{'; _print(p.first); cerr << ", "; _print(p.second); cerr << '}'; }
+template <typename T>
+void _print(vector<T> v) { cerr << "[ "; for (T i : v) _print(i), cerr << " "; cerr << "]"; }
+template <typename T>
+void _print(set<T> v) { cerr << "[ "; for (T i : v) _print(i), cerr << " "; cerr << "]"; }
+template <typename T>
+void _print(multiset<T> v) { cerr << "[ "; for (T i : v) _print(i), cerr << " "; cerr << "]"; }
+template <typename T, typename V>
+void _print(map<T, V> v) { cerr << "[ "; for (auto i : v) _print(i), cerr << " "; cerr << "]"; }
+
+/*
+ * Bakchodi Mat Kar Laude
+ * Chup Chap code kar
+ * I will not be responsible for any damage caused by this code
+ */
+
+void solve() {
+    int n,k,x;cin>>n>>k>>x;
+    if(x!=1){
+        cout<<"YES"<<endl;
+        cout<<n<<endl;
+        rep(i,0,n) cout<<1<<" ";
+        cout<<endl;
+        return;
+    }
+    else if(k==1 || (k==2 && n%2==1)){
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<"YES"<<endl;
+        cout<<n/2<<endl;
+        if (n % 2 == 1) {
+            cout << 3 << " ";
+            for (int i = 1; i < n / 2; ++i) {
+                cout << 2 << " ";
+            }
+        } else {
+            for (int i = 0; i < n / 2; ++i) {
+                cout << 2 << " ";
+            }
+        }
+        cout << "\n";
+    }
+
+}
+
+int main() {
+    fastIO();
+
+    // ✅ Always redirect stderr to Error.txt for debug
+    freopen("Error.txt", "w", stderr);
+
+#ifdef LOCAL
+    // ✅ Only redirect input/output during local debugging
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
