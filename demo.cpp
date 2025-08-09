ccbc55698f5dbac2de61d5bfcfb4a83b81e90428
aabhinavvvvvvv <abhinavguptaxia9213@gmail.com>
2025-08-10 02:14:09 +0530

    unsolved

--- demo.cpp
+++ demo.cpp
@@ -1,107 +1,37 @@
-/*
- * Competitive Programming Template
- * Author: Abhinav Gupta
- * GitHub: @aabhinavvvvvvv
- * MAHAKAL KI JAI
- */
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
+int longestSubsequence(string x, string y) {
+    int n = x.size(), m = y.size();
+    vector<vector<int>> next(n + 1, vector<int>(26, -1));
+    vector<int> last(26, -1);
 
-using ll = long long;
-using ull = unsigned long long;
-using ld = long double;
-using vi = vector<int>;
-using vll = vector<ll>;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
+    for (int i = n - 1; i >= 0; --i) {
+        last[x[i] - 'a'] = i;
+        for (int c = 0; c < 26; ++c)
+            next[i][c] = last[c];
+    }
+    for (int c = 0; c < 26; ++c)
+        next[n][c] = -1;
 
-const ll INF = 1e18;
-const int MOD = 1e9 + 7;
-const int N = 2e5 + 5;
-
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define F first
-#define S second
-#define rep(i,a,b) for(int i=a; i<b; ++i)
-#define per(i,a,b) for(int i=a; i>b; --i)
-#define each(x,a) for(auto &x : a)
-#define sz(x) (int)(x).size()
-#define fastIO() ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-
-#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
-
-void _print(int t) { cerr << t; }
-void _print(long long t) { cerr << t; }
-void _print(unsigned long long t) { cerr << t; }
-void _print(string t) { cerr << '"' << t << '"'; }
-void _print(char t) { cerr << '\'' << t << '\''; }
-void _print(long double t) { cerr << t; }
-void _print(double t) { cerr << t; }
-
-template <typename T, typename V> void _print(pair<T, V> p);
-template <typename T> void _print(vector<T> v);
-template <typename T> void _print(set<T> v);
-template <typename T> void _print(multiset<T> v);
-template <typename T, typename V> void _print(map<T, V> v);
-
-template <typename T, typename V>
-void _print(pair<T, V> p) { cerr << '{'; _print(p.first); cerr << ", "; _print(p.second); cerr << '}'; }
-template <typename T>
-void _print(vector<T> v) { cerr << "[ "; for (T i : v) _print(i), cerr << " "; cerr << "]"; }
-template <typename T>
-void _print(set<T> v) { cerr << "[ "; for (T i : v) _print(i), cerr << " "; cerr << "]"; }
-template <typename T>
-void _print(multiset<T> v) { cerr << "[ "; for (T i : v) _print(i), cerr << " "; cerr << "]"; }
-template <typename T, typename V>
-void _print(map<T, V> v) { cerr << "[ "; for (auto i : v) _print(i), cerr << " "; cerr << "]"; }
-
-/*
- * Bakchodi Mat Kar Laude
- * Chup Chap code kar
- * I will not be responsible for any damage caused by this code
- */
-
-
- void solve() {
-    int n,x;
-    cin>>n>>x;
-    vi v(n);
-    each(x,v)cin>>x;
-        for(int i=0;i<=n-x;++i){
-        bool poss=true;
-        for(int j=0;j<n;++j){
-            if(v[j] && (j<i || j>=i+x)){
-                poss=false;
-                break;
-            }
-        }
-        if(poss){
-            cout<<"YES\n";
-            return;
+    int maxLen = 0;
+    for (int i = 0; i < m; ++i) {
+        int pos = 0;
+        for (int j = i; j < m; ++j) {
+            char c = y[j];
+            if (pos >= n || next[pos][c - 'a'] == -1) break;
+            pos = next[pos][c - 'a'] + 1;
+            maxLen = max(maxLen, j - i + 1);
         }
     }
-    cout<<"NO\n";
 
-
+    return maxLen;
 }
 
 int main() {
-    fastIO();
-
-    // ✅ Always redirect stderr to Error.txt for debug
-    freopen("Error.txt", "w", stderr);
-
-#ifdef LOCAL
-    // ✅ Only redirect input/output during local debugging
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-
-    int t = 1;
-    // cin >> t;
-    while (t--) solve();
+    string x, y;
+    cin >> x >> y;
+    cout << longestSubsequence(x, y) << endl;
     return 0;
 }
