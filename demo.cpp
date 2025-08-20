44fbca4a63df4b584f9523b34f77b8fc1d9abb8e
aabhinavvvvvvv <abhinavguptaxia9213@gmail.com>
2025-08-20 23:52:25 +0530

    cc starter 200

--- demo.cpp
+++ demo.cpp
@@ -1,107 +1,65 @@
-/*
- * Competitive Programming Template
- * Author: Abhinav Gupta
- * GitHub: @aabhinavvvvvvv
- * MAHAKAL KI JAI
- */
-
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-using pii = pair<int,int>;
-
-const int N = 2e5 + 5;
-const int LOG = 20;
-
-vector<vector<pii>> up; // {ancestor, minValueOnPath}
-vector<int> depth;
-vector<vector<int>> adj;
-vector<int> arr; // values assigned to nodes
-
-void dfs(int node, int parent) {
-    // up[node][0] → (parent, minValue on 1 jump)
-    up[node][0] = { parent, (parent == -1 ? arr[node] : min(arr[node], arr[parent])) };
-
-    // fill binary lifting table for node
-    for (int j = 1; j < LOG; j++) {
-        int mid_ancestor = up[node][j - 1].first;
-        if (mid_ancestor != -1) {
-            up[node][j].first = up[mid_ancestor][j - 1].first;
-            up[node][j].second = min(up[node][j - 1].second, up[mid_ancestor][j - 1].second);
+class Solution {
+public:
+    int maxTasks(vector<int>& A, vector<int>& B, vector<int>& C) {
+        int n = A.size();
+        int maxNeed = 0;
+        for (int i = 0; i < n; i++) {
+            maxNeed = max({maxNeed, A[i], B[i]});
         }
-    }
+        int maxPoints = accumulate(C.begin(), C.end(), 0);
+        int LIM = maxNeed + maxPoints + 2;
 
-    for (auto nxt : adj[node]) {
-        if (nxt == parent) continue;
-        depth[nxt] = depth[node] + 1;
-        dfs(nxt, node);
-    }
-}
+        // dp[p][s] = maximum tasks completed with (p,s)
+        vector<vector<int>> dp(LIM+1, vector<int>(LIM+1, -1));
+        dp[1][1] = 0;
 
-pii findlca_min(int u, int v) {
-    int minVal = min(arr[u], arr[v]); // initial min is their own values
+        for (int idx = 0; idx < n; idx++) {
+            // We process each task once → like 0/1 knapsack
+            vector<vector<int>> newdp = dp;
 
-    if (depth[u] < depth[v]) swap(u, v);
-    int diff = depth[u] - depth[v];
+            for (int p = 1; p <= LIM; p++) {
+                for (int s = 1; s <= LIM; s++) {
+                    if (dp[p][s] == -1) continue;
 
-    for (int i = 0; i < LOG; ++i) {
-        if (diff & (1 << i)) {
-            minVal = min(minVal, up[u][i].second);
-            u = up[u][i].first;
+                    if (p >= A[idx] || s >= B[idx]) {
+                        int reward = C[idx];
+                        // distribute reward
+                        for (int addP = 0; addP <= reward; addP++) {
+                            int newP = min(LIM, p + addP);
+                            int newS = min(LIM, s + (reward - addP));
+                            newdp[newP][newS] = max(newdp[newP][newS], dp[p][s] + 1);
+                        }
+                    }
+                }
+            }
+            dp.swap(newdp);
         }
-    }
 
-    if (u == v) return {u, minVal};
-
-    for (int i = LOG - 1; i >= 0; --i) {
-        if (up[u][i].first != -1 && up[u][i].first != up[v][i].first) {
-            minVal = min(minVal, up[u][i].second);
-            minVal = min(minVal, up[v][i].second);
-            u = up[u][i].first;
-            v = up[v][i].first;
+        int ans = 0;
+        for (int p = 1; p <= LIM; p++) {
+            for (int s = 1; s <= LIM; s++) {
+                ans = max(ans, dp[p][s]);
+            }
         }
+        return ans;
     }
-
-    // final step: check parent of u and v
-    minVal = min(minVal, up[u][0].second);
-    minVal = min(minVal, up[v][0].second);
-
-    return { up[u][0].first, minVal };
-}
-
-void solve() {
-    int n, q;
-    cin >> n >> q;
-    arr.resize(n);
-    for (int i = 0; i < n; i++) cin >> arr[i];
-
-    adj.assign(n, {});
-    depth.assign(n, 0);
-    up.assign(n, vector<pii>(LOG, {-1, INT_MAX}));
-
-    for (int i = 0; i < n - 1; i++) {
-        int u, v; cin >> u >> v;
-        --u; --v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-
-    dfs(0, -1);
-
-    while (q--) {
-        int u, v; cin >> u >> v;
-        --u; --v;
-        pii res = findlca_min(u, v);
-        int lca = res.first;
-        int minVal = res.second;
-        int dist = depth[u] + depth[v] - 2 * depth[lca];
-        cout << dist << " " << minVal << "\n";
-    }
-}
+};
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    solve();
+    Solution sol;
+
+    vector<int> A1 = {1, 1, 2, 7};
+    vector<int> B1 = {1, 3, 4, 4};
+    vector<int> C1 = {2, 3, 1, 1};
+    cout << sol.maxTasks(A1, B1, C1) << endl; // Expected: 4
+
+    vector<int> A2 = {1, 2, 4, 9};
+    vector<int> B2 = {1, 2, 4, 9};
+    vector<int> C2 = {2, 1, 2, 3};
+    cout << sol.maxTasks(A2, B2, C2) << endl; // Expected: 3
+
+    return 0;
 }
