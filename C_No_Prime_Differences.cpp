3345305845e63aa7e5b4f10439f591c9f4c8a6cb
aabhinavvvvvvv <abhinavguptaxia9213@gmail.com>
2025-10-11 06:28:35 +0530

    m

---
+++ C_No_Prime_Differences.cpp
@@ -0,0 +1,140 @@
+/*
+ * Competitive Programming Template
+ * Author: Abhinav Gupta
+ * GitHub: @aabhinavvvvvvv
+ * MAHAKAL KI JAI
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 5;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define F first
+#define S second
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+#define per(i,a,b) for(int i=a; i>b; --i)
+#define each(x,a) for(auto &x : a)
+#define sz(x) (int)(x).size()
+#define fastIO() ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+
+void _print(int t) { cerr << t; }
+void _print(long long t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
+void _print(string t) { cerr << '"' << t << '"'; }
+void _print(char t) { cerr << '\'' << t << '\''; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+
+template <typename T, typename V> void _print(pair<T, V> p);
+template <typename T> void _print(vector<T> v);
+template <typename T> void _print(set<T> v);
+template <typename T> void _print(multiset<T> v);
+template <typename T, typename V> void _print(map<T, V> v);
+
+template <typename T, typename V>
+void _print(pair<T, V> p) { cerr << '{'; _print(p.first); cerr << ", "; _print(p.second); cerr << '}'; }
+template <typename T>
+void _print(vector<T> v) { cerr << "[ "; for (T i : v) _print(i), cerr << " "; cerr << "]"; }
+template <typename T>
+void _print(set<T> v) { cerr << "[ "; for (T i : v) _print(i), cerr << " "; cerr << "]"; }
+template <typename T>
+void _print(multiset<T> v) { cerr << "[ "; for (T i : v) _print(i), cerr << " "; cerr << "]"; }
+template <typename T, typename V>
+void _print(map<T, V> v) { cerr << "[ "; for (auto i : v) _print(i), cerr << " "; cerr << "]"; }
+
+void solve() {
+    int n, m; 
+    cin >> n >> m;
+    vector<vector<int>> grid(n, vector<int>(m));
+    
+    int val = 1;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            grid[i][j] = val++;
+        }
+    }
+
+    // debug(grid);
+
+    val = m * (n / 2) + 1;
+    rep(i, 0, m){
+        cout << val++ << " ";
+    }
+    cout << endl;
+
+    val = 1;
+    rep(i, 0, m){
+        cout << val++ << " ";
+    }
+    cout << endl;
+
+    int lastrow = n - 1;
+    int secondrow = 1;
+    int donerows[2] = {0, n / 2};
+    bool flag = true;
+
+    while (lastrow > secondrow) {
+        if (flag) {
+            if (lastrow == donerows[0] || lastrow == donerows[1]) {
+                lastrow--;
+                continue;
+            }
+            rep(i, 0, m){
+                cout << grid[lastrow][i] << " ";
+            }
+            cout << endl;
+            lastrow--;
+            flag = false;
+        } else {
+            if (secondrow == donerows[0] || secondrow == donerows[1]) {
+                secondrow++;
+                continue;
+            }
+            rep(i, 0, m){
+                cout << grid[secondrow][i] << " ";
+            }
+            cout << endl;
+            secondrow++;
+            flag = true;
+        }
+    }
+    cout << endl;
+}
+
+
+int main() {
+    fastIO();
+    #ifdef LOCAL
+    freopen("Error.txt", "w", stderr);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    auto begin = chrono::high_resolution_clock::now();
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+
+    auto end = chrono::high_resolution_clock::now();
+    auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds." << endl;
+
+    return 0;
+}
